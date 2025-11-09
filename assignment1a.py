338c55f84bdde8a3987355f9c493810515f037b4
Bala R <bala.ramkumar270@gmail.com>
2025-11-09 15:45:05 -0500

    pychanges

---
+++ assignment1a.py
@@ -0,0 +1,14 @@
+annual_salary = int(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = int(input("Enter the cost of your dream home: "))
+current_savings = 0
+portion_down_payment = 0.25
+r = 0.04
+monthly_salary = ((annual_salary/12))
+m = 0
+
+while current_savings < (portion_down_payment * total_cost):
+    current_savings += ((monthly_salary * portion_saved) + (current_savings*r/12))
+    m += 1
+
+print(m)
